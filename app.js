6bfb7dae9bdeb591e9d1db31695fddf79cf0c55a
Piotr Leniec <leniecpiotr@gmail.com>
2015-07-17 11:17:55 +0200

    connection authentication

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var server = require('http').Server(require('express')());
 var io = require('socket.io')(server);
 
-io.on('connection', function(socket) {
-  socket.emit('news', {hello: 'world'});
-  socket.on('my other event', function(data) {
-    console.log(data);
+var amqp = require('amqp').createConnection({host: 'localhost'});
+var redis = require('redis').createClient();
+
+redis.on('ready', function() {
+  amqp.on('ready', function() {
+    io.on('connection', function(socket) {
+      redis.get('access_token:' + socket.handshake.query.accessToken, function(error, userId) {
+        if(!userId) {
+          socket.disconnect();
+        }
+        else {
+          socket.emit('news', {hello: 'world'});
+          socket.on('my other event', function(data) {
+            console.log(data);
+          });
+        }
+      });
+    });
+
+    server.listen(8080);
   });
 });
-
-server.listen(8080);
