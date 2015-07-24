30c2eb2443382ad35c86dd333cfb33e2025de0c6
Piotr Leniec <leniecpiotr@gmail.com>
2015-07-24 17:15:51 +0200

    received messages from queue

--- app.js
+++ app.js
@@ -10,26 +10,29 @@ var redis = require('redis').createClient();
 var pvpc = require('./pvpc.js');
 
 redis.on('ready', function() {
+  console.log('redis ready');
   amqp.on('ready', function() {
+    console.log('amqp ready');
     io.on('connection', function(socket) {
+      console.log('connection');
       redis.get('access_token:' + socket.handshake.query.accessToken, function(error, userId) {
         if(!userId) {
+          console.log('invalid access token');
           socket.disconnect();
           return;
         }
 
+        console.log('valid access token');
         amqp.queue('chat:user:' + userId, function(queue) {
-          queue.subscribe(function(message) {
-            socket.emit('message', {message: message});
+          queue.subscribe(function(queueMessage) {
+            console.log('received message: ' + queueMessage.data.toString('utf8'));
+            socket.emit('message', {text: queueMessage.data.toString('utf8')});
           });
         });
 
         socket.on('message', function(message) {
-        });
-
-        socket.emit('news', {hello: 'world'});
-        socket.on('my other event', function(data) {
-          console.log(data);
+          redis.lrange('conversation:' + message.conversation, 0, -1, function(error, userIds) {
+          });
         });
       });
     });
