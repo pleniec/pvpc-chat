7a7ae06c5db7d977a80d841cc824268b22bee857
Piotr Leniec <leniecpiotr@gmail.com>
2015-07-27 16:43:50 +0200

    removed debug prints and fixed redis keys

--- app.js
+++ app.js
@@ -9,36 +9,33 @@ var amqp = require('amqp').createConnection({
 var redis = require('redis').createClient();
 
 redis.on('ready', function() {
-  console.log('redis ready');
   amqp.on('ready', function() {
-    console.log('amqp ready');
     io.on('connection', function(socket) {
-      console.log('connection');
       redis.get('access_token:' + socket.handshake.query.accessToken, function(error, userId) {
         if(!userId) {
-          console.log('invalid access token');
+          socket.emit('errorMessage', {text: 'invalid access token'});
           socket.disconnect();
           return;
         }
 
         socket.on('message', function(message) {
           message.userId = userId;
-          console.log('mesyndz: ' + JSON.stringify(message));
-          redis.smembers('conversation:' + message.conversation, function(error, userIds) {
+          redis.smembers('chat:conversation:' + message.conversation, function(error, userIds) {
             if(userIds.indexOf(userId) != -1) {
               userIds.forEach(function(userId) {
                 amqp.publish('chat:user:' + userId, JSON.stringify(message));
               });
             }
+            else {
+              socket.emit('errorMessage', {conversation: message.conversation,
+                                           text: 'you are not authorized to access this conversation'});
+            }
           });
         });
 
-        console.log('valid access token');
         amqp.queue('chat:user:' + userId, function(queue) {
           queue.subscribe(function(queueMessage) {
-            console.log('received message: ' + queueMessage.data.toString('utf8'));
             socket.emit('message', JSON.parse(queueMessage.data.toString('utf8')));
-            //socket.emit('message', {text: 'moze'});
           });
         });
       });
